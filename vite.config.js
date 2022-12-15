ebd23c4a903fa7bc1b42ef630e7befbe7e9cf4ab
nazareno valentini <97785887+Bazza6@users.noreply.github.com>
2022-12-15 21:01:28 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: { open: true, },
+  // server: { open: true, },
   plugins: [react()]
 })
